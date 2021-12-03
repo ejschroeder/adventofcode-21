24a92351207b69b8b8fd0b25029587ed275294e1
E.J. Schroeder <ej.schr@gmail.com>
2021-12-02 19:56:47 -0500

    start day 2

---
+++ src/Day02.kt
@@ -0,0 +1,18 @@
+data class Pos(val x: Int, val y: Int)
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+    }
+
+    fun part2(input: List<String>): Int {
+
+    }
+
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 7)
+
+    val input = readInput("Day02")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
