ae98d790b9c2a7a24f3b33074e07e962122aac0d
E.J. Schroeder <ej.schr@gmail.com>
2021-12-04 16:11:20 -0500

    add day 3 and 4

---
+++ src/Day03.kt
@@ -0,0 +1,54 @@
+fun main() {
+    fun getMostCommonBitInPosition(pos: Int, binaryStrings: List<String>): Char {
+        val freqSum = binaryStrings.fold(0) { acc, s ->
+            if (s[pos] == '1') acc + 1 else acc - 1
+        }
+
+        return if (freqSum >= 0) '1' else '0'
+    }
+
+    fun part1(input: List<String>): Int {
+        val wordLength = input.first().length
+
+        var epsilon = ""
+        var gamma = ""
+        for (idx in 0 until wordLength) {
+            val mostCommon = getMostCommonBitInPosition(idx, input)
+            val leastCommon = if (mostCommon == '1') '0' else '1'
+
+            epsilon += mostCommon
+            gamma += leastCommon
+        }
+
+        return epsilon.toInt(2) * gamma.toInt(2)
+    }
+
+    fun part2(input: List<String>): Int {
+        val wordLength = input.first().length
+        var binaryNums = input
+        for (idx in 0 until wordLength) {
+            val mostCommon = getMostCommonBitInPosition(idx, binaryNums)
+            binaryNums = binaryNums.filter { it[idx] == mostCommon }
+            if (binaryNums.size == 1) break
+        }
+        val o2Rating = binaryNums.first().toInt(2)
+
+        binaryNums = input
+        for (idx in 0 until wordLength) {
+            val leastCommon = if (getMostCommonBitInPosition(idx, binaryNums) == '1') '0' else '1'
+            binaryNums = binaryNums.filter { it[idx] == leastCommon }
+            if (binaryNums.size == 1) break
+        }
+        val co2Rating = binaryNums.first().toInt(2)
+
+        return o2Rating * co2Rating
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
+
+    val input = readInput("Day03")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
---
+++ src/Day04.kt
@@ -0,0 +1,70 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val numbers = getBingoNumbers(input)
+        var boards = getBingoBoards(input)
+
+        for (number in numbers) {
+            boards = boards.map { it.mark(number) }
+            val winningBoard = boards.firstOrNull { it.isWin() }
+            if (winningBoard != null) {
+                return winningBoard.score * number
+            }
+        }
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        val numbers = getBingoNumbers(input)
+        var boards = getBingoBoards(input)
+
+        for (number in numbers) {
+            boards = boards.map { it.mark(number) }
+
+            if (boards.size == 1) {
+                if (boards.first().isWin())
+                    return boards.first().score * number
+            } else {
+                boards = boards.filter { !it.isWin() }
+            }
+
+        }
+
+        return 0
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
+
+    val input = readInput("Day04")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
+
+fun getBingoBoards(input: List<String>) = input.asSequence()
+    .drop(1)
+    .chunked(6)
+    .map { it.drop(1) }
+    .map { it.flatMap { boardRow -> boardRow.trim().split("\\s+".toRegex()) } }
+    .map { it.map { num -> BingoCell(num.toInt()) } }
+    .map { BingoBoard(it) }
+    .toList()
+
+fun getBingoNumbers(input: List<String>) = input.first().split(",").map { it.toInt() }
+
+data class BingoCell(val value: Int, val checked: Boolean = false) {
+    fun checked() = copy(checked = true)
+}
+
+data class BingoBoard(val cells: List<BingoCell>) {
+    val score: Int
+        get() = cells.filter { !it.checked }.sumOf { it.value }
+
+    fun mark(number: Int) = BingoBoard(cells.map { if (it.value == number) it.checked() else it })
+
+    fun isWin(): Boolean {
+        return cells.chunked(5).any { row -> row.all { it.checked } }
+                || cells.withIndex().groupBy { it.index % 5 }.values.any { col -> col.all { it.value.checked } }
+    }
+}
