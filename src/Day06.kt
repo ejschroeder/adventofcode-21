e7347d99908f7991e3cdbf226635809f3112cc34
E.J. Schroeder <ej.schr@gmail.com>
2021-12-07 17:11:33 -0500

    add day 6 and 7

---
+++ src/Day06.kt
@@ -0,0 +1,41 @@
+fun getNextState(state: List<Int>): List<Int> {
+    val newCount = state.count { it == 0 }
+    return state.map { if (it == 0) 6 else it - 1 } + List(newCount) { 8 }
+}
+
+val fishCountsAtTime = mutableMapOf<Int, Long>()
+
+fun getPopulationFromNewFish(timeRemaining: Int): Long {
+    var fishCount = 1L
+    var currentTime = timeRemaining - 9
+
+    while(currentTime > 0) {
+        val childFishCount = fishCountsAtTime[currentTime] ?: getPopulationFromNewFish(currentTime)
+        fishCountsAtTime[currentTime] = childFishCount
+        fishCount += childFishCount
+        currentTime -= 7
+    }
+    return fishCount
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val startingState = input.first().split(",").map { it.toInt() }
+        val finalState = (1..80).fold(startingState) { acc, _ -> getNextState(acc) }
+        return finalState.count()
+    }
+
+    fun part2(input: List<String>): Long {
+        val startingState = input.first().split(",").map { it.toInt() }
+        return startingState.map { 256 + (9 - it) }.sumOf(::getPopulationFromNewFish)
+    }
+
+    val testInput = readInput("Day06_test")
+    println(part1(testInput))
+    check(part1(testInput) == 5934)
+    check(part2(testInput) == 26984457539L)
+
+    val input = readInput("Day06")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
---
+++ src/Day07.kt
@@ -0,0 +1,32 @@
+import kotlin.math.abs
+import kotlin.math.pow
+
+fun calcLinearFuelCost(position: Int, positions: List<Int>) = positions.sumOf { abs(it - position) }
+fun calcPolynomialFuelCost(position: Int, positions: List<Int>) = positions.sumOf {
+    val distance = abs(it - position)
+    (distance.toDouble().pow(2).toInt() + distance) / 2
+}
+
+fun main() {
+    fun part1(input: List<String>): Int? {
+        val positions = input.first().split(",").map { it.toInt() }.sorted()
+        val minPos = positions.first()
+        val maxPos = positions.last()
+        return (minPos..maxPos).map { calcLinearFuelCost(it, positions) }.minByOrNull { it }
+    }
+
+    fun part2(input: List<String>): Int? {
+        val positions = input.first().split(",").map { it.toInt() }.sorted()
+        val minPos = positions.first()
+        val maxPos = positions.last()
+        return (minPos..maxPos).map { calcPolynomialFuelCost(it, positions) }.minByOrNull { it }
+    }
+
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readInput("Day07")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
