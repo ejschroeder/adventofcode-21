8b89c75f296690565e1f0643331f0fb1ce7c3c4e
E.J. Schroeder <ej.schr@gmail.com>
2021-12-18 22:15:04 -0500

    day 18 refactor with immutable tree

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,153 +1,157 @@
 import kotlin.math.ceil
 import kotlin.math.floor
 
-sealed class SnailfishNumber {
-    class Number(var value: Int) : SnailfishNumber()
-    class Pair(var left: SnailfishNumber, var right: SnailfishNumber) : SnailfishNumber() {
-        fun magnitude(): Int {
-            val l = left
-            val r = right
+class SnailfishNumber(root: SnailfishNode.Pair) {
+    private val root: SnailfishNode.Pair
 
-            val leftMagnitude = if (l is Pair) l.magnitude() else (l as Number).value
-            val rightMagnitude = if (r is Pair) r.magnitude() else (r as Number).value
-            return (leftMagnitude * 3) + (rightMagnitude * 2)
+    init {
+        this.root = reduce(root)
+    }
+
+    fun magnitude() = root.magnitude()
+
+    companion object {
+        fun fromString(string: String): SnailfishNumber {
+            val root = parsePair(string)
+            return SnailfishNumber(root)
         }
 
-        fun parentOf(snailfishNumber: SnailfishNumber): Pair? {
-            if (left == snailfishNumber || right == snailfishNumber) return this
+        private fun parsePair(string: String): SnailfishNode.Pair {
+            val pair = string.removeSurrounding(prefix = "[", suffix = "]")
 
-            val l = left
-            if (l is Pair) {
-                val leftResult = l.parentOf(snailfishNumber)
-                leftResult?.let { return it }
-            }
+            val commaLocation = if (pair.startsWith('[')) findIndexOfClosingBrace(pair) + 1 else 1
 
-            val r = right
-            if (r is Pair) {
-                val rightResult = r.parentOf(snailfishNumber)
-                rightResult?.let { return it }
-            }
+            val leftString = pair.substring(0 until commaLocation)
+            val rightString = pair.substring(commaLocation + 1)
 
-            return null
+            val leftItem = if (leftString.startsWith("[")) parsePair(leftString) else SnailfishNode.Number(leftString.toInt())
+            val rightItem = if (rightString.startsWith("[")) parsePair(rightString) else SnailfishNode.Number(rightString.toInt())
+
+            return SnailfishNode.Pair(leftItem, rightItem)
         }
 
-        fun inorder(): List<Number> {
-            val l = left
-            val r = right
-            val leftInorder = if (l is Pair) l.inorder() else listOf(l as Number)
-            val rightInorder = if (r is Pair) r.inorder() else listOf(r as Number)
-            return leftInorder + rightInorder
-        }
+        private fun findIndexOfClosingBrace(string: String): Int {
+            if (string.first() != '[') throw Exception("Starting index is not index of opening brace")
 
-        fun inorderSearch(depth: Int = 0, predicate: (pair: Pair, depth: Int) -> Boolean, ): Pair? {
-            val l = left
-            val leftTreeResult = if (l is Pair) l.inorderSearch(depth + 1, predicate) else null
-            leftTreeResult?.let { return leftTreeResult }
-
-            val found = predicate.invoke(this, depth)
-            if (found) return this
-
-            val r = right
-            return if (r is Pair) r.inorderSearch(depth + 1, predicate) else null
-        }
-
-        fun explode(root: Pair) {
-            val explodingLeftNumber = this.left as Number
-            val explodingRightNumber = this.right as Number
-            val inorder = root.inorder()
-            val leftIndex = inorder.indexOf(explodingLeftNumber)
-            val rightIndex = inorder.indexOf(explodingRightNumber)
-            if (leftIndex - 1 in inorder.indices) {
-                inorder[leftIndex - 1].value += explodingLeftNumber.value
-            }
-            if (rightIndex + 1 in inorder.indices) {
-                inorder[rightIndex + 1].value += explodingRightNumber.value
-            }
-            val parent = root.parentOf(this)
-            if (parent != null) {
-                if (parent.left == this) {
-                    parent.left = Number(0)
-                } else {
-                    parent.right = Number(0)
+            var count = 1
+            var idx = 0
+            do {
+                idx++
+                when (string[idx]) {
+                    '[' -> count++
+                    ']' -> count--
                 }
-            }
+            } while (count != 0 && idx < string.length)
+            return if (count == 0) idx else throw Exception("Closing brace not found")
         }
 
-        fun split() {
-            if (this.left is Number && (this.left as Number).value >= 10) {
-                val num = this.left as Number
-                val half = num.value / 2.0
-                this.left = Pair(Number(floor(half).toInt()), Number(ceil(half).toInt()))
-            } else if (this.right is Number && (this.right as Number).value >= 10) {
-                val num = this.right as Number
-                val half = num.value / 2.0
-                this.right = Pair(Number(floor(half).toInt()), Number(ceil(half).toInt()))
+        private fun explode(root: SnailfishNode.Pair, pair: SnailfishNode.Pair): SnailfishNode.Pair {
+            val inorder = root.inorder()
+            val replacements = mutableMapOf<SnailfishNode, SnailfishNode>(pair to SnailfishNode.Number(0))
+
+            val explodingLeftNumber = pair.left as SnailfishNode.Number
+            val leftIndex = inorder.indexOf(explodingLeftNumber) - 1
+            if (leftIndex in inorder.indices) {
+                val number = inorder[leftIndex]
+                replacements += number to SnailfishNode.Number(number.value + explodingLeftNumber.value)
             }
+
+            val explodingRightNumber = pair.right as SnailfishNode.Number
+            val rightIndex = inorder.indexOf(explodingRightNumber) + 1
+            if (rightIndex in inorder.indices) {
+                val number = inorder[rightIndex]
+                replacements += number to SnailfishNode.Number(number.value + explodingRightNumber.value)
+            }
+
+            return root.replace(replacements) as SnailfishNode.Pair
         }
 
-        fun reduce(): Pair {
+        private fun split(root: SnailfishNode.Pair, number: SnailfishNode.Number): SnailfishNode.Pair {
+            val half = number.value / 2.0
+            val newPair = SnailfishNode.Pair(
+                left = SnailfishNode.Number(floor(half).toInt()),
+                right = SnailfishNode.Number(ceil(half).toInt())
+            )
+            return root.replace(number, newPair) as SnailfishNode.Pair
+        }
+
+        private fun reduce(root: SnailfishNode.Pair): SnailfishNode.Pair {
             var reductionFound = true
-
+            var r = root
             while(reductionFound) {
-                val explodingPair = this.inorderSearch { _, depth -> depth >= 4 }
+                val explodingPair = r.inorderSearch { _, depth -> depth >= 4 }
                 if (explodingPair != null) {
-                    explodingPair.explode(this)
+                    r = explode(r, explodingPair)
                     continue
                 }
 
-                val splittingPair = this.inorderSearch { pair, _ ->
-                    pair.left is Number && (pair.left as Number).value >= 10 ||
-                            pair.right is Number && (pair.right as Number).value >= 10
+                val splittingPair = r.inorderSearch { pair, _ ->
+                    pair.left is SnailfishNode.Number && pair.left.value >= 10 ||
+                            pair.right is SnailfishNode.Number && pair.right.value >= 10
                 }
                 if (splittingPair != null) {
-                    splittingPair.split()
+                    val number = if ((splittingPair.left is SnailfishNode.Number) && splittingPair.left.value >= 10 )
+                        splittingPair.left
+                    else
+                        splittingPair.right as SnailfishNode.Number
+                    r = split(r, number)
                     continue
                 }
 
                 reductionFound = false
             }
-
-            return this
+            return r
         }
+    }
 
-        operator fun plus(other: Pair): Pair {
-            return Pair(this, other).reduce()
+    operator fun plus(other: SnailfishNumber): SnailfishNumber {
+        val newRoot = SnailfishNode.Pair(this.root, other.root)
+        return SnailfishNumber(newRoot)
+    }
+
+
+    sealed class SnailfishNode {
+        abstract fun magnitude(): Int
+        abstract fun inorder(): List<Number>
+        abstract fun replace(target: SnailfishNode, replacement: SnailfishNode): SnailfishNode
+        abstract fun replace(targets: Map<SnailfishNode, SnailfishNode>): SnailfishNode
+
+        class Number(val value: Int) : SnailfishNode() {
+            override fun magnitude() = value
+            override fun inorder() = listOf(this)
+            override fun replace(target: SnailfishNode, replacement: SnailfishNode) = if (target == this) replacement else Number(this.value)
+            override fun replace(targets: Map<SnailfishNode, SnailfishNode>) = if (this in targets) targets[this]!! else Number(this.value)
+        }
+        class Pair(val left: SnailfishNode, val right: SnailfishNode) : SnailfishNode() {
+            override fun magnitude() = (left.magnitude() * 3) + (right.magnitude() * 2)
+            override fun inorder() = left.inorder() + right.inorder()
+
+            override fun replace(target: SnailfishNode, replacement: SnailfishNode): SnailfishNode {
+                if (this == target) return replacement
+                return Pair(left.replace(target, replacement), right.replace(target, replacement))
+            }
+
+            override fun replace(targets: Map<SnailfishNode, SnailfishNode>): SnailfishNode {
+                if (this in targets) return targets[this]!!
+                return Pair(left.replace(targets), right.replace(targets))
+            }
+
+            fun inorderSearch(depth: Int = 0, predicate: (pair: Pair, depth: Int) -> Boolean): Pair? {
+                val leftTreeResult = if (left is Pair) left.inorderSearch(depth + 1, predicate) else null
+                leftTreeResult?.let { return leftTreeResult }
+
+                val found = predicate.invoke(this, depth)
+                if (found) return this
+
+                return if (right is Pair) right.inorderSearch(depth + 1, predicate) else null
+            }
         }
     }
 }
 
 fun main() {
-    fun findIndexOfClosingBrace(string: String): Int {
-        if (string.first() != '[') throw Exception("Starting index is not index of opening brace")
-
-        var count = 1
-        var idx = 0
-        do {
-            idx++
-            when (string[idx]) {
-                '[' -> count++
-                ']' -> count--
-            }
-        } while (count != 0 && idx < string.length)
-        return if (count == 0) idx else throw Exception("Closing brace not found")
-    }
-
-    fun parsePair(string: String): SnailfishNumber.Pair {
-        val pair = string.removeSurrounding(prefix = "[", suffix = "]")
-
-        val commaLocation = if (pair.startsWith('[')) findIndexOfClosingBrace(pair) + 1 else 1
-
-        val leftString = pair.substring(0 until commaLocation)
-        val rightString = pair.substring(commaLocation + 1)
-
-        val leftItem = if (leftString.startsWith("[")) parsePair(leftString) else SnailfishNumber.Number(leftString.toInt())
-        val rightItem = if (rightString.startsWith("[")) parsePair(rightString) else SnailfishNumber.Number(rightString.toInt())
-
-        return SnailfishNumber.Pair(leftItem, rightItem)
-    }
-
     fun part1(input: List<String>): Int {
-        val finalResult = input.map(::parsePair)
+        val finalResult = input.map(SnailfishNumber::fromString)
             .reduce { acc, pair -> acc + pair }
 
         return finalResult.magnitude()
@@ -156,8 +160,8 @@ fun main() {
     fun part2(input: List<String>): Int {
         val maxMagnitude = input.elementPairs()
             .flatMap { listOf(
-                parsePair(it.first) to parsePair(it.second),
-                parsePair(it.second) to parsePair(it.first)
+                SnailfishNumber.fromString(it.first) to SnailfishNumber.fromString(it.second),
+                SnailfishNumber.fromString(it.second) to SnailfishNumber.fromString(it.first)
             )}
             .map { it.first + it.second }
             .maxOf { it.magnitude() }
